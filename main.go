4e76fdc128e3f1d7f405082753b79d142fc43cf8
thelivingbrian <brian.lynch4444@gmail.com>
2025-06-09 18:02:38 -0400

    Prototype button clicker (#1)
    * some very basic functionality
    
    * mild cleanup
    
    * move dummy code up
    
    * cleanup
    
    * basic clicker functionality  / change name
    
    * cleanup add todo
    
    * basic prototype
    
    * Marshall and merge - comment out test code
    
    * Cleanup, update todo(s)

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+
+	datastar "github.com/starfederation/datastar/sdk/go"
+)
+
+type Signal map[string]any
+
+type PageSignals struct {
+	Message    string `json:"message"`
+	Counter    int    `json:"counter"`
+	ShowDialog bool   `json:"showDialog"`
+}
+
+var (
+	tmpl   = template.Must(template.ParseGlob("templates/*.tmpl.html"))
+	views  = atomic.Int64{}
+	clicks = atomic.Int64{}
+)
+
+func main() {
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/click", click)
+	http.HandleFunc("/stream", stream)
+
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	views.Add(1)
+	tmpl.ExecuteTemplate(w, "home", nil)
+}
+
+func click(w http.ResponseWriter, r *http.Request) {
+	var signals PageSignals
+	if err := datastar.ReadSignals(r, &signals); err != nil {
+		fmt.Println(err)
+		// sse.ConsoleError(err, nil)
+		return
+	}
+
+	signals.Counter = int(clicks.Add(1))
+
+	sse := datastar.NewSSE(w, r)
+	if err := sse.MarshalAndMergeSignals(&signals); err != nil {
+		fmt.Println(err)
+		// sse.ConsoleError(err, nil)
+		return
+	}
+}
+
+func stream(w http.ResponseWriter, r *http.Request) {
+	signal := Signal{}
+	previous := int64(0)
+	ticker := time.NewTicker(100 * time.Millisecond)
+	sse := datastar.NewSSE(w, r)
+	for {
+		select {
+		case <-r.Context().Done():
+			return
+		case <-ticker.C:
+			count := clicks.Load()
+			if previous != count {
+				previous = count
+				signal["counter"] = count
+				err := sse.MarshalAndMergeSignals(&signal)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+	}
+}
