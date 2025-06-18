0e57cd375d5729979f4d1b996237e12b6d2d664d
thelivingbrian <brian.lynch4444@gmail.com>
2025-06-17 22:58:15 -0400

    disable accel buffering for metrics feed

--- server/routes.go
+++ server/routes.go
@@ -96,6 +96,7 @@ func (app *App) metricsFeed(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("X-Accel-Buffering", "no")
 
 	flusher, ok := w.(http.Flusher)
 	if !ok {
